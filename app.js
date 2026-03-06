7bbe6430be26317721c540674369e642a6ad862d
ansriaz1 <ansriaz93@gmail.com>
2026-03-06 07:04:02 +0500

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,32 @@ pages[current].date = new Date().toLocaleString();
 localStorage.setItem("pages",JSON.stringify(pages));
 updateInfo();
 }
+function uploadImage(){
 
+let input=document.createElement("input");
+
+input.type="file";
+input.accept="image/*";
+
+input.onchange=function(){
+
+let file=input.files[0];
+
+let reader=new FileReader();
+
+reader.onload=function(e){
+
+document.execCommand("insertImage",false,e.target.result);
+
+};
+
+reader.readAsDataURL(file);
+
+};
+
+input.click();
+
+  }
 function loadPages(){
 
 pagesDiv.innerHTML="";
@@ -122,3 +147,28 @@ loadPages();
 if('serviceWorker' in navigator){
 navigator.serviceWorker.register("service-worker.js");
                }
+function deletePage(){
+
+if(!confirm("Delete this page?")) return;
+
+pages.splice(current,1);
+
+if(pages.length===0){
+
+pages.push({
+title:"New Page",
+content:"",
+date:""
+});
+
+}
+
+current=0;
+
+localStorage.setItem("pages",JSON.stringify(pages));
+
+editor.innerHTML=pages[0].content;
+
+loadPages();
+
+  }
